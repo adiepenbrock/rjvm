07ea9133b4cc6ff053122e98eb6d2790d3868b52
Andreas Diepenbrock <mail@adiepenbrock.de>
2024-02-28 10:29:30 +0100

    refactor: update decoding example to use `BufferedReader`

--- examples/decoding.rs
+++ examples/decoding.rs
@@ -1,11 +1,11 @@
 use rjvm::{
-    decoder::buffer::Buffer,
     types::{constants::ConstantPool, elements::ClassFile},
+    decoder::buffer::BufferedReader,
 };
 
 fn main() {
     let input = include_bytes!("./testdata/org/example/Simple.class");
-    let mut buffer = Buffer::new(input);
+    let mut buffer = BufferedReader::new(input);
     let mut constant_pool = ConstantPool::new();
     let cf = ClassFile::decode(&mut buffer, &mut constant_pool);
 
